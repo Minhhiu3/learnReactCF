f654c38aeb6155fce33edf10b53dc48dfd49cffb
Minhhiu3 <vmink2004@gmail.com>
2025-05-21 15:47:46 +0700

    test router

---
+++ src/components/HeaderAdmin.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeaderAdmin = () => {
+	return (
+		<div>
+			<h1>Xin chào Admin,</h1>
+			<div>
+				<button>Logout</button>
+			</div>
+		</div>
+	);
+};
+
+export default HeaderAdmin;
